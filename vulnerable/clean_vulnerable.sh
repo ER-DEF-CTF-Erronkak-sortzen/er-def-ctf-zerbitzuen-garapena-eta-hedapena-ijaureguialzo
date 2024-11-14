7276c4b5a003ab2ce1e3db110591878c18ad2fd1
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-11-14 16:32:16 +0100

    Script de limpieza

--- vulnerable/clean_vulnerable.sh
+++ vulnerable/clean_vulnerable.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-docker stop vulnerable_ssh_1
-docker stop vulnerable_web_1
-docker rm vulnerable_ssh_1
-docker rm vulnerable_web_1
+docker rm -fv vulnerable-phpmyadmin-1
+docker rm -fv vulnerable-web-1
+docker rm -fv vulnerable-db-1
+docker volume rm -f vulnerable_mariadb-data
+docker network rm -f vulnerable_default
