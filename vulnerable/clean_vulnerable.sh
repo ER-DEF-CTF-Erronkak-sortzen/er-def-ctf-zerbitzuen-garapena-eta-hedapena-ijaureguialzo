3eb3dedca0546e51777626a0534b4427d020bd0a
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-11-15 00:00:53 +0100

    Opción no disponible

--- vulnerable/clean_vulnerable.sh
+++ vulnerable/clean_vulnerable.sh
@@ -4,4 +4,4 @@ docker rm -fv vulnerable_phpmyadmin_1
 docker rm -fv vulnerable_web_1
 docker rm -fv vulnerable_db_1
 docker volume rm -f vulnerable_mariadb-data
-docker network rm -f vulnerable_default
+docker network rm vulnerable_default
